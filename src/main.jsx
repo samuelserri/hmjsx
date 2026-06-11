447f16d335d6014b81a8824a9b5b178eba2546a2
Jonatan Serri <samuelcerri@gmail.com>
2026-06-11 09:48:43 -0400

    att

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Home from './pages/Home.jsx'
-import './style.css'
+import '../style.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
